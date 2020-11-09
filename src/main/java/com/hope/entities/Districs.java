f8371f13758fb5c37781779ec066364988849752
phamlonganv <66854890+phamlonganv@users.noreply.github.com>
2020-11-09 11:03:17 +0700

    abc

--- src/main/java/com/hope/entities/Districs.java
+++ src/main/java/com/hope/entities/Districs.java
@@ -12,7 +12,7 @@ import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "quan")
+@Table(name = "districs")
 public class Districs {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
